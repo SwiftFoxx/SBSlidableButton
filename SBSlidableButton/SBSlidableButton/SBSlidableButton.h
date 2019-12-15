6be5c1a3aab3551fb19e898c6f6c0042d75bda27
Koushik Mudi <km.kaushik92@gmail.com>
2019-12-16 01:57:25 +0530

    Xcode project
    Created the Xode project to start with

---
+++ SBSlidableButton/SBSlidableButton/SBSlidableButton.h
@@ -0,0 +1,19 @@
+//
+//  SBSlidableButton.h
+//  SBSlidableButton
+//
+//  Created by Koushik Mudi on 16/12/19.
+//  Copyright © 2019 Koushik Mudi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SBSlidableButton.
+FOUNDATION_EXPORT double SBSlidableButtonVersionNumber;
+
+//! Project version string for SBSlidableButton.
+FOUNDATION_EXPORT const unsigned char SBSlidableButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SBSlidableButton/PublicHeader.h>
+
+
